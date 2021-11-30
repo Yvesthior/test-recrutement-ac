54fdf188e5e941949a908d31249af4125776008c
Yvesthior <yvesthior@gmail.com>
2021-11-30 10:51:20 +0000

    database creation

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index() {
-        $films = Film::all();
+        $films = Film::with('genre')->paginate(10);
 
         return view('index', compact('films'));
     }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,16 +8,30 @@
     <div class="film_area">
 
         <section class="cards">
-            @foreach($films as $film)
+            {{--@foreach($films as $film)
             <div class="card">
-                <img src="storage/img_avatar.png" alt="Avatar" style="width:100%">
-                <div class="container">
-                    <h4><b>{{$film->name}}</b></h4>
-                    <p>Architect & Engineer</p>
-                </div>
+                <img src="storage/{{$film->image}}" alt="Avatar" style="width:100%">
+                <h1>{{$film->name}}</h1>
+                <p class="title">{{$film->genre->name}}</p>
+                <br>
+                <p><button>Ajouter aux favoris</button></p>
             </div>
+            @endforeach--}}
+            @foreach($films as $film)
+                <div class="card">
+                    <div class="card__header">
+                        <img src="storage/{{$film->image}}" alt="card__image" class="card__image" width="600">
+                    </div>
+                    <div class="card__body">
+                        <span class="tag tag-blue">{{$film->genre->name}}</span>
+                        <h4>{{$film->name}}</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!</p>
+                    </div>
+                </div>
             @endforeach
-
         </section>
+    </div>
+    <div class="pagination">
+        {{ $films->links() }}
     </div>
 @endsection
